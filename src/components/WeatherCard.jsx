588aa599c4b3c59c9f7384930284037a11e1e2de
Pako Cárdenas <pakorro2@gmail.com>
2022-09-27 00:07:29 -0500

    ultimos cambios

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -2,30 +2,36 @@ import React from 'react'
 import { useState } from 'react'
 
 
+
 const WeatherCard = ({ weather }) => {
 
-  let icon = `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`
+  let icon = `https://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`
   let unitTempC = Math.trunc(weather?.main.temp)
   let unitTempF = Math.trunc(unitTempC * 1.8 + 32)
   const changeTemperature = () => setChangeTemp(!changeTemp)
   const [changeTemp, setChangeTemp] = useState(true)
 
+
   return (
     <article className='card'>
       <h1>Weather App</h1>
       <section className='card__city' >
-        <h2 className='card__city-title' >{`${weather?.name}, ${weather?.sys.country}`}</h2>
-        <h2 className='card__city-temperature'>Temperature {changeTemp ? `${unitTempC} °C` : `${unitTempF} °F`}</h2>
-      </section>
-      <section className='card__weather-time'>
+        <h2 className='card__city-title' >
+          {`${weather?.name}, ${weather?.sys.country}`}
+        </h2>
         <img src={icon} alt="icon-weather" className='card__weather-icon' />
+        <h2 className='card__state-weather'>{weather?.weather[0].description}</h2>
         <ul className='card__list-description'>
-          <h2>{weather?.weather[0].description}</h2>
-          <li>Wind Speed: {weather?.wind.speed} m/s</li>
-          <li>Clouds: {weather?.clouds.all} %</li>
-          <li>Pressure: {weather?.main.pressure} hPa</li>
+          <li>
+            <span>Wind Speed:</span> {weather?.wind.speed} m/s
+          </li>
+          <li><span>Clouds:</span> {weather?.clouds.all} %</li>
+          <li><span>Pressure:</span> {weather?.main.pressure} hPa</li>
         </ul>
       </section>
+      <h2 className='card__city-temperature'>
+        {changeTemp ? `${unitTempC} °C` : `${unitTempF} °F`}
+      </h2>
       <button className='card__btn' onClick={changeTemperature}>
         {changeTemp ? `Show in °F` : `Show in °C`}
       </button>
